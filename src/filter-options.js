15a2fdb13a3e0ebf2783a373b7b91ecafe9fff98
brother-ur <verenikin.dmitrij@gmail.com>
2022-03-23 21:01:27 +0300

    add: add zips codes

--- src/filter-options.js
+++ src/filter-options.js
@@ -18,281 +18,495 @@ export const BRANDS = [
 ]
 
 export const TECHIQUE = [
-{
-  value: 'refrigerator',
-  label: 'Refrigerator'
-},
-{
-  value: 'freezer',
-  label: 'Freezer',
-},
-{
-  value: 'dryer',
-  label: 'Dryer'
-},
-{
-  value: 'wine-cooler',
-  label: 'Wine cooler'
-},
   {
-  value: 'microwave',
-  label: 'Microwave'
-},
-{
-  value: 'range',
-  label: 'Range',
-},
-{
-  value: 'cooktop',
-  label: 'Cooktop'
-},
-{
-  value: 'vent-hood',
-  label: 'Vent hood'
-},
-{
-  value: 'dishwasher',
-  label: 'Dishwasher'
-},
-{
-  value: 'washer',
-  label: 'Washer',
-},
-{
-  value: 'dryer',
-  label: 'Dryer'
-},
-{
-  value: 'garbage-disposal',
-  label: 'Garbage disposal'
-},
-]
-
-
-export const ZIP_CODES =  [
-  {
-    "zip_code": 11933,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Calverton",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'refrigerator',
+    label: 'Refrigerator'
   },
   {
-    "zip_code": 11934,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Center Moriches",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'freezer',
+    label: 'Freezer',
   },
   {
-    "zip_code": 11935,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Cutchogue",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'dryer',
+    label: 'Dryer'
   },
   {
-    "zip_code": 11937,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "East Hampton",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'wine-cooler',
+    label: 'Wine cooler'
+  },
+    {
+    value: 'microwave',
+    label: 'Microwave'
   },
   {
-    "zip_code": 11939,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "East Marion",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'range',
+    label: 'Range',
   },
   {
-    "zip_code": 11940,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "East Moriches",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'cooktop',
+    label: 'Cooktop'
   },
   {
-    "zip_code": 11941,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Eastport",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'vent-hood',
+    label: 'Vent hood'
   },
   {
-    "zip_code": 11942,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "East Quogue",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'dishwasher',
+    label: 'Dishwasher'
   },
   {
-    "zip_code": 11943,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Greenport",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'washer',
+    label: 'Washer',
   },
   {
-    "zip_code": 11944,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Greenport",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'dryer',
+    label: 'Dryer'
   },
   {
-    "zip_code": 11946,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Hampton Bays",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11947,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Jamesport",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11948,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Laurel",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11949,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Manorville",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11950,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Mastic",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11951,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Mastic Beach",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11952,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Mattituck",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11953,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Middle Island",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11954,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Montauk",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11955,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Moriches",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11956,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "New Suffolk",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11957,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Orient",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11958,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Peconic",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11959,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Quogue",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11960,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Remsenburg",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11961,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Ridge",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11962,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Sagaponack",
-    "state": "NY",
-    "county": "Suffolk"
-  },
-  {
-    "zip_code": 11963,
-    "latitude": 40.922326,
-    "longitude": -72.637078,
-    "city": "Sag Harbor",
-    "state": "NY",
-    "county": "Suffolk"
+    value: 'garbage-disposal',
+    label: 'Garbage disposal'
   },
 ]
 
+export const ZIP_CODES = [
+  {
+    "zip_code": '94022',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94023',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94024',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94025',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94026',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94027',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94028',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94029',
+    "city": 'Los Altos'
+  },
+  {
+    "zip_code": '94035',
+    "city": 'Mountain View'
+  },
+  {
+    "zip_code": '94039',
+    "city": 'Mountain View'
+  },
+  {
+    "zip_code": '94040',
+    "city": 'Mountain View'
+  },
+  {
+    "zip_code": '94041',
+    "city": 'Mountain View'
+  },
+  {
+    "zip_code": '94042',
+    "city": 'Mountain View'
+  },
+  {
+    "zip_code": '94043',
+    "city": 'Mountain View'
+  },
+  {
+    "zip_code": '94061',
+    "city": 'Woodside'
+  },
+  {
+    "zip_code": '94062',
+    "city": 'Emerald Hills'
+  },
+  {
+    "zip_code": '94062',
+    "city": 'Redwood City'
+  },
+  {
+    "zip_code": '94063',
+    "city": 'Redwood City'
+  },
+  {
+    "zip_code": '94064',
+    "city": 'Redwood City'
+  },
+  {
+    "zip_code": '94065',
+    "city": 'Redwood City'
+  },
+  {
+    "zip_code": '94084',
+    "city": 'Sunnyvale'
+  },
+  {
+    "zip_code": '94085',
+    "city": 'Sunnyvale'
+  },
+  {
+    "zip_code": '94086',
+    "city": 'Sunnyvale'
+  },
+  {
+    "zip_code": '94087',
+    "city": 'Sunnyvale'
+  },
+  {
+    "zip_code": '94088',
+    "city": 'Sunnyvale'
+  },
+  {
+    "zip_code": '94089',
+    "city": 'Sunnyvale'
+  },
+  {
+    "zip_code": '94301',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '94302',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '94303',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '94304',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '94305',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '94306',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '94309',
+    "city": 'Palo Alto'
+  },
+  {
+    "zip_code": '95002',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95008',
+    "city": 'Campbell'
+  },
+  {
+    "zip_code": '95009',
+    "city": 'Campbell'
+  },
+  {
+    "zip_code": '95011',
+    "city": 'Campbell'
+  },
+  {
+    "zip_code": '95014',
+    "city": 'Cupertino'
+  },
+  {
+    "zip_code": '95015',
+    "city": 'Cupertino'
+  },
+  {
+    "zip_code": '95030',
+    "city": 'Los Gatos'
+  },
+  {
+    "zip_code": '95031',
+    "city": 'Los Gatos'
+  },
+  {
+    "zip_code": '95032',
+    "city": 'Los Gatos'
+  },
+  {
+    "zip_code": '95033',
+    "city": 'Los Gatos'
+  },
+  {
+    "zip_code": '95035',
+    "city": 'Milpitas'
+  },
+  {
+    "zip_code": '95036',
+    "city": 'Milpitas'
+  },
+  {
+    "zip_code": '95050',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95051',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95052',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95053',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95054',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95055',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95056',
+    "city": 'Santa Clara'
+  },
+  {
+    "zip_code": '95070',
+    "city": 'Saratoga'
+  },
+  {
+    "zip_code": '95071',
+    "city": 'Saratoga'
+  },
+  {
+    "zip_code": '95101',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95103',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95106',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95108',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95109',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95110',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95111',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95112',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95113',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95115',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95116',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95117',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95118',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95119',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95120',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95121',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95122',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95123',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95124',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95125',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95126',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95127',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95128',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95129',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95130',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95131',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95132',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95133',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95134',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95135',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95136',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95138',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95139',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95148',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95150',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95151',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95152',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95153',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95154',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95155',
+    "city": 'San Jose'  
+  },
+  {
+    "zip_code": '95156',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95157',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95158',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95159',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95160',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95161',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95164',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95170',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95172',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95173',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95190',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95191',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95192',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95193',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95194',
+    "city": 'San Jose'
+  },
+  {
+    "zip_code": '95196',
+    "city": 'San Jose'
+  }
+];
