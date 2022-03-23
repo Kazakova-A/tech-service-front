713bac27f987957a4a541f7311aa810d92416af2
brother-ur <verenikin.dmitrij@gmail.com>
2022-03-23 20:20:08 +0300

    add: add techs

--- src/filter-options.js
+++ src/filter-options.js
@@ -1,266 +1,298 @@
 export const BRANDS = [
-    {
-        value: 'apple',
-        label: 'Apple'
-    },
-    {
-        value: 'Samsung',
-        label: 'Samsung'
-    },
-    {
-        value: 'lg',
-        label: 'LG'
-    },
-    {
-        value: 'phillips',
-        label: 'Phillips'
-    },
+  {
+      value: 'apple',
+      label: 'Apple'
+  },
+  {
+      value: 'Samsung',
+      label: 'Samsung'
+  },
+  {
+      value: 'lg',
+      label: 'LG'
+  },
+  {
+      value: 'phillips',
+      label: 'Phillips'
+  },
 ]
 
 export const TECHIQUE = [
+{
+  value: 'refrigerator',
+  label: 'Refrigerator'
+},
+{
+  value: 'freezer',
+  label: 'Freezer',
+},
+{
+  value: 'dryer',
+  label: 'Dryer'
+},
+{
+  value: 'wine-cooler',
+  label: 'Wine cooler'
+},
   {
-    value: 'TV',
-    label: 'TV'
-  },
-  {
-    value: 'phone',
-    label: 'Phone',
-  },
-  {
-    value: 'computer',
-    label: 'Computer'
-  },
-  {
-    value: 'refrigerator',
-    label: 'Refrigerator'
-  },
+  value: 'microwave',
+  label: 'Microwave'
+},
+{
+  value: 'range',
+  label: 'Range',
+},
+{
+  value: 'cooktop',
+  label: 'Cooktop'
+},
+{
+  value: 'vent-hood',
+  label: 'Vent hood'
+},
+{
+  value: 'dishwasher',
+  label: 'Dishwasher'
+},
+{
+  value: 'washer',
+  label: 'Washer',
+},
+{
+  value: 'dryer',
+  label: 'Dryer'
+},
+{
+  value: 'garbage-disposal',
+  label: 'Garbage disposal'
+},
 ]
 
 
 export const ZIP_CODES =  [
-    {
-      "zip_code": 11933,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Calverton",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11934,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Center Moriches",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11935,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Cutchogue",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11937,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "East Hampton",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11939,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "East Marion",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11940,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "East Moriches",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11941,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Eastport",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11942,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "East Quogue",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11943,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Greenport",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11944,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Greenport",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11946,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Hampton Bays",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11947,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Jamesport",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11948,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Laurel",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11949,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Manorville",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11950,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Mastic",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11951,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Mastic Beach",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11952,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Mattituck",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11953,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Middle Island",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11954,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Montauk",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11955,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Moriches",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11956,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "New Suffolk",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11957,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Orient",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11958,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Peconic",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11959,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Quogue",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11960,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Remsenburg",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11961,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Ridge",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11962,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Sagaponack",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-    {
-      "zip_code": 11963,
-      "latitude": 40.922326,
-      "longitude": -72.637078,
-      "city": "Sag Harbor",
-      "state": "NY",
-      "county": "Suffolk"
-    },
-  ]
-  
+  {
+    "zip_code": 11933,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Calverton",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11934,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Center Moriches",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11935,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Cutchogue",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11937,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "East Hampton",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11939,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "East Marion",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11940,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "East Moriches",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11941,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Eastport",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11942,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "East Quogue",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11943,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Greenport",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11944,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Greenport",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11946,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Hampton Bays",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11947,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Jamesport",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11948,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Laurel",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11949,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Manorville",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11950,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Mastic",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11951,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Mastic Beach",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11952,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Mattituck",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11953,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Middle Island",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11954,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Montauk",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11955,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Moriches",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11956,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "New Suffolk",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11957,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Orient",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11958,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Peconic",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11959,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Quogue",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11960,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Remsenburg",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11961,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Ridge",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11962,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Sagaponack",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+  {
+    "zip_code": 11963,
+    "latitude": 40.922326,
+    "longitude": -72.637078,
+    "city": "Sag Harbor",
+    "state": "NY",
+    "county": "Suffolk"
+  },
+]
+
