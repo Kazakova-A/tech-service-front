34ec17123e46558188c6dcb78fc59cd6625ac1e0
Oleg Zakhzrov <oleg.zakharov.1416@gmail.com>
2022-04-28 14:15:27 +0300

    add: menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { memo } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { configureStore, history } from './store/store';
+import Menu from './pages/Menu';
 import Home from './pages/Home';
+import Jobs from './pages/Jobs';
+import Employees from './pages/Employees';
 import Notification from './components/Notification';
 
 const store = configureStore();
@@ -13,10 +16,15 @@ function App() {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Route path="/">
-          <Home />
-          <Notification />
-        </Route>
+        <Menu />
+        <Switch>
+          <Route path="/employees"><Employees /></Route>
+          <Route path="/jobs"><Jobs /></Route>
+          <Route path="/">
+            <Home />
+            <Notification />
+          </Route>
+        </Switch>
       </ConnectedRouter>
     </Provider>
   );
